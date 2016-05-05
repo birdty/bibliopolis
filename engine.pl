c7dd4104a813ce47286bd864595c43d3ee79deda
Tyler Bird <birdty@gmail.com>
2016-05-05 16:23:34 -0400

    o - no comment

--- engine.pl
+++ engine.pl
@@ -6,61 +6,77 @@ package main;
 
 use CGI;
 use IO::File;
+use Error qw(:try);
 
-run();
-
-sub run 
+try
 {
-  my $cgi = CGI->new();
+  run();
 
-  require Bibliopolis::Site::Administrative::FrontController;
-
-  my $parameters = {};
-
-  foreach my $param_name ( $cgi->param() )
+  sub run 
   {
-      my $value = $cgi->param($param_name);
+    my $cgi = CGI->new();
 
-      $parameters->{$param_name} = $value;
-  }
+    require Bibliopolis::Site::Administrative::FrontController;
 
-  # cgi will be an implementation detail
-  # for example one use case is an action will
-  # call the front controller to get the location of an uploaded file
-  # this will be wrapped and no knowledge of cgi will be exposed to the action 
-  # that is called from the front controller.
+    my $parameters = {};
 
-  my $console;
-  my $view_type;
+    foreach my $param_name ( $cgi->param() )
+    {
+	my $value = $cgi->param($param_name);
 
-  if ( $ENV{'HTTP_HOST'} ) {
-    require Bibliopolis::Console::HTML;
+	$parameters->{$param_name} = $value;
+    }
+
+    # cgi will be an implementation detail
+    # for example one use case is an action will
+    # call the front controller to get the location of an uploaded file
+    # this will be wrapped and no knowledge of cgi will be exposed to the action 
+    # that is called from the front controller.
+
+    my $console;
+    my $view_type;
+
+    if ( $ENV{'HTTP_HOST'} ) {
+      require Bibliopolis::Console::HTML;
+      
+      my $fh = new IO::File "console_template.html", "r";
+
+      my $html_template = join("\n", <$fh>);
+
+      undef $fh;
+
+      $console = Bibliopolis::Console::HTML->new($html_template);
+      
+      $view_type = 'html';
+
+    }
+    else
+    {
+	# command line console used when running unit tests.
+    }
     
-    my $fh = new IO::File "console_template.html", "r";
 
-    my $html_template = join("\n", <$fh>);
+    my $fc = Bibliopolis::Site::Administrative::FrontController->new({
+	  'parameters'	=> $parameters,
+	  'console'	=> $console,
+	  'cgi'		=> $cgi,
+	  'view_type'	=> $view_type
+    });
 
-    undef $fh;
-
-    $console = Bibliopolis::Console::HTML->new($html_template);
-    
-    $view_type = 'html';
-
+    $fc->process_request();
   }
-  else
-  {
-      # command line console used when running unit tests.
-  }
-  
 
-  my $fc = Bibliopolis::Site::Administrative::FrontController->new({
-	'parameters'	=> $parameters,
-	'console'	=> $console,
-	'cgi'		=> $cgi,
-	'view_type'	=> $view_type
-  });
-
-  $fc->process_request();
 }
+catch Error with
+{
+  my $error = shift;
+
+  use Data::Dumper;
+
+  print("Content-type: text/html\n\n");
+  
+  print Dumper(\$error);
+
+};
 
 1;
